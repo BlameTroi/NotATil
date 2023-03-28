98dcd9930df9b0a7aa9ca9e11028fd99480737a4
troi <blametroi@gmail.com>
2023-03-28 14:44:50 -0400

    pulled new code from testtok over, still need to create tokenizer in notatil

--- testtok.scm
+++ testtok.scm
@@ -28,81 +28,16 @@
 ;; develop and test in one file :)
 ;;
 
-(define nat-buffer "")
-(define nat-tokenized '())
-(define nat-compiling #f)
-(define nat-in-def #f)
-(define nat-in-comment #f)
-(define nat-in-string #f)
-(define nat-buffer-empty #t)
-
-;; some of the forms we'll have to deal with
+;; moved to notatil.scm
 ;;
-;; USE xxx
-;; USING xxx Use file xxx as Forth “disk”
-;; ( xxx)	Ignores text up to “)” delimeter
-;; INCLUDE xxx Load the text file xxx
-;; FORGET xxx	Forget definitions back through xxx
-;; MARKER xxx	Defines marker xxx to roll back dictionary
-;;            where xxx is a word that forgets itself
-;; : ;
-;; variable xxx
-;; constant xxx
-;; cells
-;; allot
+;; (define nat-buffer "")
+;; (define nat-tokenized '())
+;; (define nat-compiling #f)
+;; (define nat-in-def #f)
+;; (define nat-in-comment #f)
+;; (define nat-in-string #f)
+;; (define nat-buffer-empty #t)
 
-
-;; these gymnastics are needed because I haven't figured
-;; out how to enter (char-set #\" #\( #\) ) with lispy.
-;; I'd rather not switch it off and on.
-(define dlm-lsc (string->list "\"()"))
-(define dlm-quote (car dlm-lsc))
-(define dlm-lparen (cadr dlm-lsc))
-(define dlm-rparen (caddr dlm-lsc))
-(define dlm-openers (char-set dlm-quote dlm-lparen))
-(define dlm-closers (char-set dlm-quote dlm-rparen))
-
-
-;; original simple scrub
-(define (nat-scrub s)
-  "Normalize string S for tokenization. Change all white-
-space to blanks, reduce runs of blanks to a single blank
-unless they are in a Forth string or comment, and add
-blanks to both ends of the incoming string to simplify
-boundary handling."
-  (let ((accum '(#\space))
-        (lastc #\space) (currc #\space)
-        (inside #f) (seeking #\nul)
-        (t (string->list s)))
-    (while (not (null? t))
-      ;; consume next char
-      (set! lastc currc) (set! currc (car t)) (set! t (cdr t))
-      ;; if inside pass straight thru but watch for end
-      (if inside
-          (begin      ; just pass inside through
-            (set! accum (cons currc accum))
-            (if (char=? seeking currc)
-                (begin                  ; transition to outside
-                  (set! seeking #\nul)
-                  (set! inside #f)))
-            (continue)))
-      ;;
-      (if (or (char=? currc dlm-lparen) (char=? currc dlm-quote))
-          (begin                ; entered a string or comment
-            (set! accum (cons currc accum))
-            (set! inside #t)
-            (set! seeking (if (char=? currc dlm-lparen) dlm-rparen dlm-quote))
-            (continue)))
-      ;;
-      (if (or (char=? currc #\nl) (char=? currc #\tab))
-          (set! currc #\space))
-      ;;
-      (if (or (not (char=? currc #\space)) (not (char=? lastc currc)))
-          (set! accum (cons currc accum))))
-    ;; end of while, watch for needing one more space
-    (if (not (char=? (car accum) #\space))
-        (set! accum (cons #\space accum)))
-    (list->string (reverse accum))))
 
 
 (define (tok-tester s)
@@ -115,8 +50,8 @@ NotaTil."
   (set! nat-buffer (nat-scrub s))
   (set! nat-tokenized '())
   (set! nat-compiling #f)
-  (set! nat-in-comment #f)
-  (set! nat-in-string #f)
+  ;; (set! nat-in-comment #f)
+  ;; (set! nat-in-string #f)
   (set! nat-buffer-empty #f)
   ;; call net-tokenizer until such time as nat-buffer
   ;; is empty
@@ -157,130 +92,6 @@ NotaTil."
       (set! tok-closing-pair '())))
   ;; put in proper order for caller
   (reverse nat-tokenized))
-
-(define (nat-next-token)
-  "Get the next token out of nat-buffer and return it as
-a (type . text) pair. Updates tokenizer state and buffer
-but does not consume past the token.
-
-Anything not recognized is assumed to be a word and will
-be addressed later in notatil."
-  (nat-trimleft-buffer)
-  (cond (nat-buffer-empty
-         (set! nat-buffer "")
-         (cons 'nat-tok-none ""))
-        ((string= "" nat-buffer)
-         (set! nat-buffer-empty #t)
-         (cons 'nat-tok-none ""))
-        (else
-         (cons (nat-buffer-token-code) (nat-buffer-to-blank)))))
-
-
-(define (nat-buffer-token-code)
-  "Does the first token in the buffer exist in the code
-mapping table? If so, return the code or unknown."
-  (letrec*
-      ((f (lambda (xs)
-            (cond ((null? xs) 'nat-tok-word)
-                  ((nat-buffer-prefix? (car (car xs))) (cdr (car xs)))
-                  (else (f (cdr xs))))
-            )))
-    (f nat-token-code-map)))
-
-;;;
-;;; Buffer parse and carve
-;;;
-
-(define (nat-trimleft-buffer)
-  "Remove leading blanks from nat-buffer. Recursive."
-  (cond ((or nat-buffer-empty (= 0 (string-length nat-buffer)))
-         (set! nat-buffer-empty #t)     ; emptied
-         (set! nat-buffer ""))
-        ((not (char=? #\space (string-ref nat-buffer 0) )))
-        (else
-         (set! nat-buffer (substring nat-buffer 1)) ; trim and check again
-         (nat-trimleft-buffer))))
-
-(define (nat-buffer-to-blank)
-  "Take the next token from nat-buffer up to a blank
-or the end of the buffer. The scrubbing process when
-starting tokenize is expected to wrap the buffer with
-a blank on either end.
-
-The terminating blank is consumed but not returned."
-  (let* ((i (string-index nat-buffer #\space))
-         (t (substring nat-buffer 0 i)))
-    (set! nat-buffer (substring nat-buffer (1+ i)))
-    t))
-
-(define (nat-buffer-to-char c)
-  "Return the contents of the buffer up to but not
-including character C. This would be useful when
-finding end of string, etc. The terminating character
-is not returned or consumed."
-  (let ((i (string-index nat-buffer c)) (t ""))
-    (cond (i (set! t (substring nat-buffer 0 i))
-             (set! nat-buffer (substring nat-buffer i)))
-          (else (set! t (string-join (list  "ERROR COULD NOT FIND EXPECTED '" (list->string (list c)) "'") ""))))
-    t))
-
-(define (nat-buffer-through-char c)
-  "Return the contents of the buffer up to and
-including character C."
-  (error "not implemented"))
-
-(define (nat-buffer-to-token tk)
-  "Return the contents of the buffer up to but not
-including the string token TK."
-  (error "not implemented"))
-
-(define (nat-buffer-through-token tk)
-  "Return the contents of the buffer up to and
-including the string token TK."
-  (error "not implemented"))
-
-(define (nat-buffer-prefix? s)
-  "DRY buffer checks."
-  (= (string-length s) (string-prefix-length-ci nat-buffer s)))
-
-;;
-;; Token and type mapping table
-;;
-;; Not every possible word is included. Control structure
-;; and definition words are, plus some repl operations.
-;;
-(define nat-token-code-map
-  (list (cons "do " 'nat-tok-do)
-        (cons "loop " 'nat-tok-loop)
-        (cons "if " 'nat-tok-if)
-        (cons "then " 'nat-tok-then)
-        (cons "else " 'nat-tok-else)
-        (cons ": " 'nat-tok-definition)
-        (cons "; " 'nat-tok-end-definition)
-        (cons "variable " 'nat-tok-variable)
-        (cons "constant " 'nat-tok-constant)
-        (cons "marker " 'nat-tok-marker)
-        (cons "see " 'nat-tok-see)
-        (cons "list " 'nat-tok-list)
-        (cons "bye " 'nat-tok-bye)
-        (cons "help " 'nat-tok-help)
-        (cons "( " 'nat-tok-begin-comment)
-        (cons ") " 'nat-tok-end-comment)
-        ;; strings can be dormant or printing
-        ;; they are closed by the first quote
-        ;; after the open, not space quote, so
-        ;; that space would be in the string.
-        ;; 'nat-tok-string-end can't be in
-        ;; this table because some forths
-        ;; allow a single quote to start a
-        ;; string.
-        (cons ".\" " 'nat-tok-print-string)
-        (cons "s\" " 'nat-tok-begin-string)
-        (cons "\" " 'nat-tok-begin-string)
-        ;;
-        ;;
-        ;;
-        ))
 
 
 ;;;
